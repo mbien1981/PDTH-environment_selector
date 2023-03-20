b02db1da9d5f3aac1bb9e231764e64c6900ebb3c
Uziel <clauselpitoloko@gmail.com>
2023-03-20 17:50:31 +0100

    Baguettefication

--- mod_localization.lua
+++ mod_localization.lua
@@ -2,10 +2,12 @@ local module = ... or D:module("environment_selector")
 
 module:add_localization_string("es_loc_default_environment", {
 	english = "Default Environment",
+	french = "Météo par défaut",
 })
 
 module:add_localization_string("es_loc_random_environment", {
 	english = "Random Environment",
+	french = "Météo aléatoire",	
 })
 
 
@@ -18,7 +20,6 @@ local levels = {
 	"slaughter_house",
 	"suburbia",
 	"secret_stash",
-	"hospital",
 }
 local level_names = {
 	english = {
@@ -30,30 +31,31 @@ local level_names = {
 		["slaughter_house"] = "Slaughter House",
 		["suburbia"] = "Counterfeit",
 		["secret_stash"] = "Undercover",
-		["hospital"] = "No Mercy",
 	},
 	french = {
 		["bank"] = "First World Bank",
-		["heat_street"] = "Heat Street",
-		["apartment"] = "Panic Room",
-		["bridge"] = "Green Bridge",
-		["diamond_heist"] = "Diamond Heist",
-		["slaughter_house"] = "Slaughter House",
-		["suburbia"] = "Counterfeit",
-		["secret_stash"] = "Undercover",
-		["hospital"] = "No Mercy",
+		["heat_street"] = "Ca chauffe dans la rue",
+		["apartment"] = "Pièce de sûreté",
+		["bridge"] = "Pont Vert",
+		["diamond_heist"] = "Vol de diamants",
+		["slaughter_house"] = "Abattoir",
+		["suburbia"] = "Contrefacon",
+		["secret_stash"] = "En sous-marin",
 	},
 }
 
 for _, level in pairs(levels) do
 	module:add_localization_string("es_loc_" .. level .. "_environment", {
 		english = string.format("%s Environment", level_names["english"][level]),
+		french = string.format("Météo de %s", level_names["english"][level]),		
 	})
 
 	module:add_localization_string("es_loc_rain_on_" .. level, {
 		english = string.format("Rain on %s", level_names["english"][level]),
+		french = string.format("La pluie sur %s", level_names["english"][level]),		
 	})
 	module:add_localization_string("es_loc_lightning_on_" .. level, {
 		english = string.format("Lightnings on %s", level_names["english"][level]),
+		french = string.format("Lumières sur %s", level_names["english"][level]),		
 	})
 end
